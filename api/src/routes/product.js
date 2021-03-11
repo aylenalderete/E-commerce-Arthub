ef34bd7290faff9793dc0b6102d5dc50d3c1d59f
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-11 16:26:46 -0300

    ruta producto y cambio modelo categoria

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,50 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const { Product, Image, Category } = require('../db.js');
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
+// Get all products 
+server.get('/', (req, res) => {
+	Product.findAll({
+		include: [
+			{
+				model: Image,
+				required: true
+			},
+			{
+				model: Category,
+				required: true
+			}
+		]
+	})
 		.then(products => {
 			res.send(products);
 		})
-		.catch(next);
+		.catch(() => {
+			res.status(500).json({ message: 'Internal server error' })
+		});
+});
+
+// Create a new product
+server.post('/', async function (req, res) {
+	const { title, price, description, stock, image, category } = req.body;
+	try {
+		const newProduct = await Product.create({
+			title,
+			price,
+			description,
+			stock
+		})
+		const productImage = await Image.create({
+			url: image
+		})
+		const categories = category;
+
+		await newProduct.setCategories(categories)
+		await newProduct.setImages(productImage)
+		res.json('New product added')
+	}
+	catch (err) {
+		res.status(500).json({ message: 'Error' })
+	}
 });
 
 module.exports = server;
