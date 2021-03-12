861d325bfbb27df6c7a756ab5f42eca4b99c0725
Flor <inquires.flo@gmail.com>
2021-03-11 21:29:45 -0300

    Home view, carousel component y un poco de css

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
+import Carrousel from '../Components/Carrousel/carrousel';
+import Styles from './home.module.css'
 
 function Home() {
     return (
         <div>
+            
             <NavBar renderTop={true} />
+            <div className={Styles.titleContainer}>
+                <h1 className={Styles.title}>arthub</h1>
+                <p className={Styles.subtitle}>art on the spotlight and stuff</p>
+            </div>
+            <Carrousel></Carrousel>
         </div>
     )
 }
