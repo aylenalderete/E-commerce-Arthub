0760d292ad92ab180d6ecab10ab55d3a5e9be1f0
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-13 17:39:05 -0300

    detalles de usuario por id

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,16 +1,28 @@
 const server = require('express').Router();
-const { User } = require('../db.js');
+const { User, Category, Image } = require('../db.js');
 
 // Get all users
 server.get('/', (req, res) => {
 
 	User.findAll({
-		attributes : ['id' ,'username', 'name', 'lastname', 'type' ]
+		attributes: ['id', 'username', 'name', 'lastname', 'birth', 'type', 'email']
 	})
-	.then(result => {
-		res.json(result)
-	})
+		.then(result => {
+			res.json(result)
+		})
 
+})
+
+// Get user by id
+server.get('/:id', (req, res) => {
+
+	User.findAll({
+		where: { id: req.params.id },
+		attributes: ['id', 'username', 'name', 'lastname', 'birth', 'type', 'email']
+	})
+		.then(result => {
+			res.json(result)
+		})
 })
 
 module.exports = server;
