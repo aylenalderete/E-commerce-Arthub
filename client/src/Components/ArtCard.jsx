0b1e147afb7a3787e477e8cec417cf3da647381d
San <santilongueirapp@gmail.com>
2021-03-13 16:40:58 -0300

    commiting some routing, a minor fix to collection, took out the hardcoding from the artpiece and the artcard

--- client/src/Components/ArtCard.jsx
+++ client/src/Components/ArtCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import style from './artcard.module.css'
 import {Link} from 'react-router-dom'
 
-function ArtCard({name, pic, artist}) {
+function ArtCard({name, pic, artist, id}) {
     return (
       <div className={style.cardContainer}>
         <div className={style.imgContainer}>
           <img className={style.cardImg} alt="artpic" src={pic}></img>
         </div>
         <div className={style.linksArtCard}>
-          <Link className={style.linksA} to={`/colección/${name}`}>
+          <Link className={style.linksA} to={`/colección/${id}`}>
             <h5 className={style.text}>{name}</h5>
           </Link>
           <Link  className={style.linksA} to={`/artists/${artist}`}>
--- client/src/Views/ArtPiece.jsx
+++ client/src/Views/ArtPiece.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import art from '../fakeList'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 import style from './artpiece.module.css'
 
-function ArtPiece({artName}) {
 
-    var pieceFinder = art.find(x => x.name === artName)
-    return (
+function ArtPiece({artId}) {
+
+const [detailed, setDetailed] = useState({
+  name: '',
+  description: '',
+  stock: 0,
+  images: [{
+    url: ''
+  }]
+});
+
+useEffect(() => {
+axios
+  .get(`http://localhost:3001/products/${artId}`)
+  .then((result) => setDetailed(result.data));  
+}, [])
+
+return (
       <div className={style.container}>
-        {/* <div className={style.imgContainer}>
-          <img className={style.img} alt="artpic" src={pieceFinder.pic}></img>
+        <div className={style.imgContainer}>
+          <img className={style.img} alt="artpic" src={detailed.images[0].url}></img>
         </div>
         <div className={style.infoContainer}>
           <div className={style.artistContainer}>
-            <h3>{pieceFinder.artist}</h3>
-            <h4>{pieceFinder.rating}</h4>
+            {/* <h3>{detailed.artist}</h3> */}
+            <h4>{detailed.stock}</h4>
           </div>
           <div>
-            <h1>{pieceFinder.name}</h1>
+            <h1>{detailed.name}</h1>
           </div>
           <div>
-            <p>{pieceFinder.description}</p>
+            <p>{detailed.description}</p>
             <button className={style.button}>añadir a mi compra</button>
           </div>
-        </div> */}
+        </div>
       </div>
     );
 }
