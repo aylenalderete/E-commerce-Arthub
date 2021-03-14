0d2eb6cb7f1bc3247ba4f25105087af2b310b036
Liza Saravia <liza.saraviag@gmail.com>
2021-03-14 11:15:06 -0300

    Arreglo css artistas y form categorias

--- client/src/Views/Artists.jsx
+++ client/src/Views/Artists.jsx
@@ -5,6 +5,9 @@ import NavBar from '../Components/NavBar'
 import style from './artists.module.css'
 import {useDispatch, useSelector} from 'react-redux'
 import getUsersArtists from '../Actions/getUsersArtists'
+import PopUp from '../Components/PopUpFilters/PopUp';
+import PopUpSort from '../Components/PopUpSort/popUpSort';
+
 
 function Artists() {
 
@@ -15,18 +18,38 @@ function Artists() {
         dispatch(getUsersArtists());
     }, [])
 
+    const [filter, setFilter] = useState(false);
+
+    const showFilters = () => {
+      filter === false ?
+        setFilter(true) :
+        setFilter(false);
+    }
+  
+    const [order, setOrder] = useState(false);
+    const showOrder = () => {
+      order === false ?
+        setOrder(true) :
+        setOrder(false);
+    }
+
     return (
         <div className = {style.mainContainerArtist}>
             <NavBar renderTop={false} />                
             <div className={style.secondContainer}>
+                {filter === true ? <PopUp /> : <></>}
+                {order === true ? <PopUpSort order={true} /> : <></>}
+                
                 <div className={style.sbContainer}>
+                    <button className={style.btnFilters} onClick={showFilters}>filtrar</button>
+                    <button className={style.btnFilters} onClick={showOrder}>ordenar</button>
                     <SearchBar />
                 </div>
             
                 <div className={style.artists}>
                 {usersArtists&&usersArtists.map((artist) => (
                     <ArtistCard 
-                        artist = {artist.username}
+                        name = {artist.username}
                         ></ArtistCard>
                     ))}
                 </div>
