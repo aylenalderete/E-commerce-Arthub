5a8d6d32fa1e2c21a02ab5ce64a1f2cfdc3ec952
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-17 21:45:01 -0300

    imagen-categoria

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -1,17 +1,20 @@
 const { DataTypes } = require('sequelize');
 // Modelo de categoria
 module.exports = (sequelize) => {
-	sequelize.define('category', {
-		name: {
+    sequelize.define('category', {
+        name: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
-		},
+        },
         description: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        
-    },{timestamps: false})
-
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+        { timestamps: false })
 };
