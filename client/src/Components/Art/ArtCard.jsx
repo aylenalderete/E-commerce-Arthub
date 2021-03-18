df6fec046e6831570718aebd35d74d67a59c8e14
Flor <inquires.flo@gmail.com>
2021-03-18 14:12:15 -0300

    user views

--- client/src/Components/Art/ArtCard.jsx
+++ client/src/Components/Art/ArtCard.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import style from './artcard.module.css';
 import { Link } from 'react-router-dom';
-import userLog from '../../Actions/userLog';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector } from 'react-redux';
+import editPiece from '../../Images/edit.svg';
+import deletePiece from '../../Images/delete.svg';
+
 
 function ArtCard({ name, pic, artist, id, idArtist, admin, price }) {
- 
-  
+  const userType = useSelector(state => state.userData.type);
 
-  return (
-    <div className={style.cardContainer}>
-      <div className={style.imgContainer}>
-        <img className={style.cardImg} alt="artpic" src={pic}></img>
+ // if user is unlogged or buyer type
+  if (!userType || userType === 'user') {
+    return (
+      <div className={style.cardContainer}>
+        <div className={style.imgContainer}>
+          <img className={style.cardImg} alt="artpic" src={pic}></img>
+        </div>
+        <div className={style.linksArtCard}>
+          <Link className={style.linksA} to={`/coleccion/${id}`}>
+            <h5 className={style.text}>{name}</h5>
+          </Link>
+          <h5 className={style.text}>{"$ " + price}</h5>
+          <Link className={style.linksA} to={`/artistas/${idArtist}`}>
+            <h5 className={style.text}>{artist}</h5>
+          </Link>
+          <Link className={style.linksA} to='/carrito'>
+            <p className={style.btn}>Añadir al carrito</p>
+          </Link>
+        </div>
       </div>
-      <div className={style.linksArtCard}>
-        <Link className={style.linksA} to={`/coleccion/${id}`}>
-          <h5 className={style.text}>{name}</h5>
-        </Link>
-        <h5 className={style.text}>{"$ " + price}</h5>
-        <Link className={style.linksA} to={`/artistas/${idArtist}`}>
-          <h5 className={style.text}>{artist}</h5>
-        </Link>
-        {
-          admin && <Link to={`editarproducto/${id}`}>Editar</Link>
-        }
-        <Link className={style.linksA} to= '/carrito'>
-          <p className={style.btn}>Añadir al carrito</p>
-        </Link>
+    );
+  }
+
+// if user is artist type 
+  else if (userType === 'artist'){
+    return (
+      <div className={style.cardContainer}>
+        <div className={style.imgContainer}>
+          <img className={style.cardImg} alt="artpic" src={pic}></img>
+          <Link to={`/editarproducto/${id}`} className={style.btnEdit}>
+            <img className={style.icon} src={editPiece} alt='edit item'/>
+          </Link>
+            <Link to={`/editarproducto/${id}`} className={style.btnDelete}>
+            <img className={style.icon} src={deletePiece} alt='delete item'/>
+            </Link>
+        </div>
+        <div className={style.linksArtCard}>
+          <Link className={style.linksA} to={`/coleccion/${id}`}>
+            <h5 className={style.pieceName}>Pieza: {name}</h5>
+          </Link>
+          <h5 className={style.text}>Precio: {"$ " + price}</h5>
+          <Link className={style.linksA} to={`/artistas/${idArtist}`}>
+            <h5 className={style.text}>Artista: {artist}</h5>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ArtCard
