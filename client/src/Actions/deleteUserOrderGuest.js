9c909da888045e1aafa0f014ff9969c237f416a4
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-23 00:59:27 -0300

    carrito

--- client/src/Actions/deleteUserOrderGuest.js
+++
@@ -1,6 +0,0 @@
-
-export default function deleteUserOrderGuest() {
-    return{
-        type: 'DELETE_USER_ORDER_GUEST'
-    }
-}
--- client/src/Components/LineOrder/LineOrder.jsx
+++ client/src/Components/LineOrder/LineOrder.jsx
@@ -1,73 +1,14 @@
 import React, { useEffect, useState } from "react";
 import style from "./lineOrder.module.css";
-import userLog from "../../Actions/userLog";
 import { useSelector, useDispatch } from "react-redux";
-import getUserOrder from "../../Actions/getUserOrder";
-import deleteUserOrder from "../../Actions/deleteUserOrder";
-import changeQuantity from "../../Actions/changeQuantity.js";
-import changeQuantityGuest from "../../Actions/changeQuantityGuest";
-import deleteLineOrderGuest from "../../Actions/deteleLineOrderGuest";
 
 export default function LineOrder({ lineOrderElement, change }) {
-	const isUserLogged = useSelector((state) => state.isUserLogged);
-	const shoppingCart = useSelector((state) => state.shoppingCart);
-	const userData = useSelector((state) => state.userData);
-	const dispatch = useDispatch();
 
-	const handleDeleteUserOrder = async (idorder, idlineorder, idUser) => {
-		if (userData.username) {
-			await dispatch(deleteUserOrder(idorder, idlineorder));
-			dispatch(getUserOrder(idUser));
-		} else {
-			let cart = JSON.parse(localStorage.getItem("cart"));
-			cart = cart.filter(
-				(l) =>
-					l.product.id_product !== lineOrderElement.product.id_product
-			);
-			localStorage.setItem("cart", JSON.stringify(cart));
-			dispatch(deleteLineOrderGuest(lineOrderElement.product.id_product));
+	// const isUserLogged = useSelector((state) => state.isUserLogged);
+	// const shoppingCart = useSelector((state) => state.shoppingCart);
+	// const userData = useSelector((state) => state.userData);
+	// const dispatch = useDispatch();
 
-			// change();
-		}
-	};
-
-	const handleQuantity = async (idUser, idlineorder, quantity) => {
-		if (userData.username) {
-			await dispatch(changeQuantity(idUser, idlineorder, quantity));
-			dispatch(getUserOrder(idUser));
-		} else {
-			if (quantity <= lineOrderElement.product.stock && quantity >= 1) {
-				let cart = JSON.parse(localStorage.getItem("cart"));
-				lineOrderElement.quantity = quantity;
-				cart = cart.filter(
-					(l) =>
-						l.product.id_product !==
-						lineOrderElement.product.id_product
-				);
-				cart.push(lineOrderElement);
-
-				cart.sort(function (a, b) {
-					if (a.product.id_product > b.product.id_product) {
-						return 1;
-					}
-					if (a.product.id_product < b.product.id_product) {
-						return -1;
-					}
-					// a must be equal to b
-					return 0;
-				});
-				localStorage.setItem("cart", JSON.stringify(cart));
-				// change();
-				// console.log(lineOrderElement);
-				dispatch(
-					changeQuantityGuest(
-						lineOrderElement.product.id_product,
-						quantity
-					)
-				);
-			}
-		}
-	};
 
 	return (
 		<div className={style.card}>
@@ -82,48 +23,48 @@ export default function LineOrder({ lineOrderElement, change }) {
 
 			<div>
 				<button className={style.btn}
-					onClick={() =>
-						handleQuantity(
-							userData.id,
-							lineOrderElement.id_line,
-							lineOrderElement.quantity - 1
-						)
-					}
+				// onClick={() =>
+				// 	// handleQuantity(
+				// 	// 	userData.id,
+				// 	// 	lineOrderElement.id_line,
+				// 	// 	lineOrderElement.quantity - 1
+				// 	// )
+				// }
 				>
 					-
 				</button>
 				<button className={style.btn}>{lineOrderElement.quantity}</button>
 				<button className={style.btn}
-					onClick={() =>
-						handleQuantity(
-							userData.id,
-							lineOrderElement.id_line,
-							lineOrderElement.quantity + 1
-						)
-					}
+				// onClick={() =>
+				// 	handleQuantity(
+				// 		userData.id,
+				// 		lineOrderElement.id_line,
+				// 		lineOrderElement.quantity + 1
+				// 	)
+				// }
 				>
 					+
 				</button>
 			</div>
 			<div>
-			<button className={style.btn}
-				onClick={() =>
-					handleDeleteUserOrder(
-						shoppingCart.id_order,
-						lineOrderElement.id_line,
-						userData.id
-					)
-				}
-			>
-				X
+				<button className={style.btn}
+				// onClick={() =>
+				// 	handleDeleteUserOrder(
+				// 		shoppingCart.id_order,
+				// 		lineOrderElement.id_line,
+				// 		userData.id
+				// 	)
+				// }
+				>
+					X
 			</button>
 			</div>
-			<p className = {style.text}>{lineOrderElement.unit_price}</p>
-			<p className = {style.text}>
+			<p className={style.text}>{lineOrderElement.unit_price}</p>
+			{/* <p className = {style.text}>
 				{lineOrderElement.quantity * lineOrderElement.unit_price < 0
 					? 0
 					: lineOrderElement.quantity * lineOrderElement.unit_price}
-			</p>
+			</p> */}
 		</div>
 	);
 }
