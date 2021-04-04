9b5e48f6d800e5469da80fc8f98e2dda96ecad38
San <santilongueirapp@gmail.com>
2021-04-03 21:18:28 -0300

    autocomplete searchbar

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -22,7 +22,7 @@ const initialState = {
     artistsProducts: [],
     //filters states
     isActiveFilters: false,
-
+    suggestions: [],
     // Carrito
 
     cart: JSON.parse(localStorage.getItem('cart')) || [],
@@ -87,373 +87,371 @@ const initialState = {
 const reducer = function (state = initialState, action) {
 
     switch (action.type) {
-        //aca crear los switch cases de cada action
-        case "GET_AUCTION_VIEW":
-            return {
-                ...state,
-                auctionView : action.payload
-            };
-        case "GET_PRODUCT_REVIEWS":
-            return {
-                ...state,
-                reviewsProduct: action.payload,
-            };
-        case "ADD_PRODUCT_REVIEW":
-            return {
-                ...state,
-                messages: action.payload,
-            };
+      //aca crear los switch cases de cada action
+      case "GET_AUCTION_VIEW":
+        return {
+          ...state,
+          auctionView: action.payload,
+        };
+      case "GET_PRODUCT_REVIEWS":
+        return {
+          ...state,
+          reviewsProduct: action.payload,
+        };
+      case "ADD_PRODUCT_REVIEW":
+        return {
+          ...state,
+          messages: action.payload,
+        };
 
-        case "GET_USER_REVIEWS":
-            return {
-                ...state,
-                userReviews: action.payload
-            }
+      case "GET_USER_REVIEWS":
+        return {
+          ...state,
+          userReviews: action.payload,
+        };
 
-        case 'DELETE_PRODUCT_REVIEW':
-            return {
-                ...state,
-                messages: action.payload
-            }
+      case "DELETE_PRODUCT_REVIEW":
+        return {
+          ...state,
+          messages: action.payload,
+        };
 
-        case 'UPDATE_PRODUCT_REVIEW':
-            return {
-                ...state,
-                userReviews: action.payload
-            }
+      case "UPDATE_PRODUCT_REVIEW":
+        return {
+          ...state,
+          userReviews: action.payload,
+        };
 
-        case "GET_PRODUCTS":
-            if (state.filteredProducts.length > 0 && !state.search[0]) {
-                return {
-                    ...state,
-                    filteredProducts: action.payload,
-                };
-            }
-            return {
-                ...state,
-                search: action.payload,
-            };
+      case "GET_PRODUCTS":
+        if (state.filteredProducts.length > 0 && !state.search[0]) {
+          return {
+            ...state,
+            filteredProducts: action.payload,
+          };
+        }
+        return {
+          ...state,
+          search: action.payload,
+        };
 
-        case "GET_INITIAL_PRODUCTS":
-            return {
-                ...state,
-                products: action.payload,
-            };
+      case "GET_SUGGESTIONS":
+        
+          return {
+            ...state,
+            suggestions: action.payload,
+          };
+        
+      case "GET_INITIAL_PRODUCTS":
+        return {
+          ...state,
+          products: action.payload,
+        };
 
-        case "SET_URL_IMAGES":
-            return {
-                ...state,
-                urlImages: action.payload,
-            };
+      case "SET_URL_IMAGES":
+        return {
+          ...state,
+          urlImages: action.payload,
+        };
 
-        case "GET_CATEGORIES":
-            return {
-                ...state,
-                categories: action.payload,
-            };
+      case "GET_CATEGORIES":
+        return {
+          ...state,
+          categories: action.payload,
+        };
 
-        case "SET_FILTERS":
-            return {
-                ...state,
-                filteredProducts: action.payload,
-            };
+      case "SET_FILTERS":
+        return {
+          ...state,
+          filteredProducts: action.payload,
+        };
 
-        case "SHOW_FILTERS":
-            return {
-                ...state,
-                isOpenFilters: action.payload,
-            };
+      case "SHOW_FILTERS":
+        return {
+          ...state,
+          isOpenFilters: action.payload,
+        };
 
-        case "SIGN_OUT":
-            return {
-                ...state,
-                userData: {
-                    id: 0,
-                    username: "",
-                    name: "",
-                    lastname: "",
-                    email: "",
-                    birth: "",
-                    type: "",
-                    state: "",
-                },
-                shoppingCart: [],
-            };
+      case "SIGN_OUT":
+        return {
+          ...state,
+          userData: {
+            id: 0,
+            username: "",
+            name: "",
+            lastname: "",
+            email: "",
+            birth: "",
+            type: "",
+            state: "",
+          },
+          shoppingCart: [],
+        };
 
-        case "GET_USER_PRODUCTS":
-            return {
-                ...state,
-                products: action.payload,
-            };
+      case "GET_USER_PRODUCTS":
+        return {
+          ...state,
+          products: action.payload,
+        };
 
-        case "GET_USERS_ARTISTS":
-            return {
-                ...state,
-                usersArtists: action.payload,
-            };
+      case "GET_USERS_ARTISTS":
+        return {
+          ...state,
+          usersArtists: action.payload,
+        };
 
-        case "GET_ARTISTS_PRODUCTS":
-            return {
-                ...state,
-                artistsProducts: action.payload,
-            };
+      case "GET_ARTISTS_PRODUCTS":
+        return {
+          ...state,
+          artistsProducts: action.payload,
+        };
 
-        case "CLEAR_URL_IMAGES":
-            return {
-                ...state,
-                urlImages: [],
-            };
+      case "CLEAR_URL_IMAGES":
+        return {
+          ...state,
+          urlImages: [],
+        };
 
-        case "SEARCH_FILTERS":
-            if (state.search[0]) {
-                let searchF = state.search.filter((f) =>
-                    f.categories.find((x) => x.name === action.payload)
-                );
-                if (!searchF[0]) {
-                    searchF = "void";
-                }
-                return {
-                    ...state,
-                    search: searchF,
-                };
-            }
+      case "SEARCH_FILTERS":
+        if (state.search[0]) {
+          let searchF = state.search.filter((f) =>
+            f.categories.find((x) => x.name === action.payload)
+          );
+          if (!searchF[0]) {
+            searchF = "void";
+          }
+          return {
+            ...state,
+            search: searchF,
+          };
+        }
 
-            return {
-                ...state,
-            };
+        return {
+          ...state,
+        };
 
-        case "SIGN_IN":
-            if (action.payload.auth === true)
-                return {
-                    ...state,
-                    userData: action.payload.user,
-                };
-            else
-                return {
-                    ...state,
-                };
+      case "SIGN_IN":
+        if (action.payload.auth === true)
+          return {
+            ...state,
+            userData: action.payload.user,
+          };
+        else
+          return {
+            ...state,
+          };
 
-        case "SIGN_IN_REFRESH":
-            return {
-                ...state,
-                userData: action.payload,
-            };
+      case "SIGN_IN_REFRESH":
+        return {
+          ...state,
+          userData: action.payload,
+        };
 
-        case "MOVE_CAROUSEL":
-            if (action.payload === "next" && state.carouselActive < 3) {
-                return {
-                    ...state,
-                    carouselActive: state.carouselActive + 1,
-                };
-            }
+      case "MOVE_CAROUSEL":
+        if (action.payload === "next" && state.carouselActive < 3) {
+          return {
+            ...state,
+            carouselActive: state.carouselActive + 1,
+          };
+        }
 
-            if (action.payload === "prev" && state.carouselActive > 1) {
-                return {
-                    ...state,
-                    carouselActive: state.carouselActive - 1,
-                };
-            }
+        if (action.payload === "prev" && state.carouselActive > 1) {
+          return {
+            ...state,
+            carouselActive: state.carouselActive - 1,
+          };
+        }
 
-            if (action.payload === "next" && state.carouselActive === 3) {
-                return {
-                    ...state,
-                    carouselActive: 1,
-                };
-            }
+        if (action.payload === "next" && state.carouselActive === 3) {
+          return {
+            ...state,
+            carouselActive: 1,
+          };
+        }
 
-            if (action.payload === "prev" && state.carouselActive === 1) {
-                return {
-                    ...state,
-                    carouselActive: 3,
-                };
-            }
+        if (action.payload === "prev" && state.carouselActive === 1) {
+          return {
+            ...state,
+            carouselActive: 3,
+          };
+        }
 
-        case "AUTOPLAY":
-            return {
-                ...state,
-                autoplay: action.payload,
-            };
+      case "AUTOPLAY":
+        return {
+          ...state,
+          autoplay: action.payload,
+        };
 
-        case "IS_USER_LOGGED":
-            return {
-                ...state,
-                isUserLogged: action.payload,
-            };
+      case "IS_USER_LOGGED":
+        return {
+          ...state,
+          isUserLogged: action.payload,
+        };
 
-        case "POP_UP_CATEGORY":
-            return {
-                ...state,
-                isOpenCategory: action.payload,
-            };
+      case "POP_UP_CATEGORY":
+        return {
+          ...state,
+          isOpenCategory: action.payload,
+        };
 
-        case "POP_UP_DELETE_CATEGORY":
-            return {
-                ...state,
-                isOpenDeleteCat: action.payload,
-            };
-        case "POP_UP_DELETE_PRODUCT":
-            return {
-                ...state,
-                isOpenDeleteProd: action.payload,
-            };
+      case "POP_UP_DELETE_CATEGORY":
+        return {
+          ...state,
+          isOpenDeleteCat: action.payload,
+        };
+      case "POP_UP_DELETE_PRODUCT":
+        return {
+          ...state,
+          isOpenDeleteProd: action.payload,
+        };
 
+      case "PRODUCT_ID":
+        return {
+          ...state,
+          productId: action.payload,
+        };
 
+      case "GET_USER_ORDERS":
+        return {
+          ...state,
+          userOrders: action.payload,
+        };
 
-        case "PRODUCT_ID":
-            return {
-                ...state,
-                productId: action.payload,
-            };
+      case "RESET_CAROUSEL":
+        return {
+          ...state,
+          carouselActive: 1,
+        };
+      case "ACTIVE_FILTERS":
+        return {
+          ...state,
+          isActiveFilters: action.payload,
+        };
 
-        case "GET_USER_ORDERS":
-            return {
-                ...state,
-                userOrders: action.payload,
-            };
+      case "GET_ALL_USERS":
+        return {
+          ...state,
+          users: action.payload,
+        };
 
+      case "POP_UP_PROMOTE_USER":
+        return {
+          ...state,
+          promoteUser: action.payload,
+        };
 
+      case "POP_UP_DELETE_USER":
+        return {
+          ...state,
+          deleteUser: action.payload,
+        };
 
+      case "ADD_ITEM_POPUP":
+        if (action.payload) {
+          var array = [...state.selectedCategories];
+          array.push(action.payload);
+        } else {
+          console.log("no deberia entrar aca");
+          var array = [];
+        }
+        return {
+          ...state,
+          selectedCategories: array,
+        };
 
+      case "DELETE_ITEM_POPUP":
+        return {
+          ...state,
+          selectedCategories: [
+            ...state.selectedCategories.filter((n) => n != action.payload),
+          ],
+        };
 
+      // Cart
+      case "ADD_ITEM":
+        // let index = state.cart.indexOf(action.payload)
+        const found = state.cart.find(
+          (f) => f.product.id_product === action.payload.product.id_product
+        );
+        if (!found) {
+          return {
+            ...state,
+            cart: [...state.cart, action.payload],
+          };
+        } else {
+          return {
+            ...state,
+            cart: state.cart.map((p) => {
+              if (p.product.id_product === action.payload.product.id_product) {
+                p = action.payload;
+              }
+              return p;
+            }),
+          };
+        }
 
+      case "REDUCE_QUANTITY":
+        let change = state.cart.map((c) => {
+          if (action.payload === c.product.id_product && c.quantity > 1) {
+            c.quantity -= 1;
+            c.subTotal = c.quantity * c.product.price;
+          }
+          return c;
+        });
+        localStorage.setItem("cart", JSON.stringify(change));
 
+        return {
+          ...state,
+          cart: change,
+        };
 
-        case "RESET_CAROUSEL":
-            return {
-                ...state,
-                carouselActive: 1,
-            };
-        case "ACTIVE_FILTERS":
-            return {
-                ...state,
-                isActiveFilters: action.payload,
-            };
+      case "DELETE_ITEM":
+        let filter = state.cart.filter(
+          (f) => action.payload !== f.product.id_product
+        );
+        localStorage.setItem("cart", JSON.stringify(filter));
+        return {
+          ...state,
+          cart: filter,
+        };
 
-        case "GET_ALL_USERS":
-            return {
-                ...state,
-                users: action.payload,
-            };
+      case "EMPTY_CART":
+        localStorage.setItem("cart", JSON.stringify([]));
+        return {
+          ...state,
+          cart: [],
+        };
 
-        case "POP_UP_PROMOTE_USER":
-            return {
-                ...state,
-                promoteUser: action.payload,
-            };
+      case "LINK_SET":
+        return {
+          ...state,
+          linkmp: action.payload,
+        };
 
-        case "POP_UP_DELETE_USER":
-            return {
-                ...state,
-                deleteUser: action.payload,
-            };
+      case "GET_ARTIST_SALES":
+        return {
+          ...state,
+          artistSales: action.payload,
+        };
+      case "GET_AUCTIONS":
+        return {
+          ...state,
+          auctions: action.payload,
+        };
 
-        case "ADD_ITEM_POPUP":
-            if (action.payload) {
-                var array = [...state.selectedCategories];
-                array.push(action.payload);
-            } else {
-                console.log("no deberia entrar aca");
-                var array = [];
-            }
-            return {
-                ...state,
-                selectedCategories: array,
-            };
+      case "POP_UP_CREATE_AUCTION":
+        return {
+          ...state,
+          createAuction: action.payload,
+        };
 
-        case "DELETE_ITEM_POPUP":
-            return {
-                ...state,
-                selectedCategories: [
-                    ...state.selectedCategories.filter((n) => n != action.payload),
-                ],
-            };
+      case "POP_UP_DELETE_AUCTION":
+        return {
+          ...state,
+          deleteAuction: action.payload,
+        };
 
-        // Cart
-        case "ADD_ITEM":
-            // let index = state.cart.indexOf(action.payload)
-            const found = state.cart.find((f) => f.product.id_product === action.payload.product.id_product)
-            if (!found) {
-                return {
-                    ...state,
-                    cart: [...state.cart, action.payload]
-                }
-            } else {
-                return {
-                    ...state,
-                    cart: state.cart.map(p => {
-                        if (p.product.id_product === action.payload.product.id_product) {
-                            p = action.payload
-                        }
-                        return p
-                    })
-                }
-            }
-
-        case 'REDUCE_QUANTITY':
-
-            let change = state.cart.map(c => {
-                if (action.payload === c.product.id_product && c.quantity > 1) {
-                    c.quantity -= 1
-                    c.subTotal = c.quantity * c.product.price
-                }
-                return c
-            })
-            localStorage.setItem('cart', JSON.stringify(change))
-
-            return {
-                ...state,
-                cart: change
-            }
-
-        case 'DELETE_ITEM':
-            let filter = state.cart.filter(f => action.payload !== f.product.id_product)
-            localStorage.setItem('cart', JSON.stringify(filter));
-            return {
-                ...state,
-                cart: filter
-            }
-
-        case 'EMPTY_CART':
-            localStorage.setItem('cart', JSON.stringify([]));
-            return {
-                ...state,
-                cart: []
-            }
-
-        case 'LINK_SET':
-
-            return {
-                ...state,
-                linkmp: action.payload
-            }
-
-
-        case 'GET_ARTIST_SALES':
-            return {
-                ...state,
-                artistSales: action.payload
-}
-        case 'GET_AUCTIONS':
-            return {
-                ...state,
-                auctions: action.payload
-            }
-
-        case 'POP_UP_CREATE_AUCTION':
-            return {
-                ...state,
-                createAuction: action.payload
-            }
-
-        case 'POP_UP_DELETE_AUCTION':
-            return {
-                ...state,
-                deleteAuction:action.payload
-
-            }
-
-        default:
-            return state;
+      default:
+        return state;
     }
 
 };
